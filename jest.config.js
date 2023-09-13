584dd6e6779a6c8a4a710b6e0542dc1618a4fe8a
Alison Lemos <alisongarantizado@gmail.com>
2023-09-13 16:23:29 -0400

    test: adding missing test configurations

--- jest.config.js
+++ jest.config.js
@@ -12,8 +12,13 @@ const config = {
   collectCoverageFrom: [
     "<rootDir>/src/**/*.{ts,tsx}",
   ],
+  coveragePathIgnorePatterns: [
+    "<rootDir>/src/main.tsx",
+  ],
   moduleNameMapper: {
+    "^.+\\.(jpg|jpeg|png|gif|webp|avif|svg|ttf|woff|woff2)$": `<rootDir>/__mocks__/fileMock.ts`,
     "^.+\\.module\\.(css|sass|scss|less)$": "identity-obj-proxy",
+    "^.+\\.(css|sass|scss|less)$": "<rootDir>/__mocks__/styleMock.ts",
     "^@/(.*)$": "<rootDir>/src/$1",
   },
   resetMocks: true
